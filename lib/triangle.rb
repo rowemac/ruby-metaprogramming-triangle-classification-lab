76c115c11197e1d769324c17d035d8f65f88a730
Mac Rowe <m.rowe42@gmail.com>
2021-03-10 15:53:14 -0500

    commit tree 5e5337335e07b5479f6b21168ddce617081b16a5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles, :scalene, :side_1, :side_2, :side_3, :type
+
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @type = kind 
+  end
+
+  def kind
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      begin
+        raise TriangleError
+      end
+    elsif side_1 + side_2 <= side_3 || 
+          side_2 + side_3 <= side_1 ||
+          side_1 + side_3 <= side_2
+      begin
+        raise TriangleError
+          end
+    elsif side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 != side_2 && side_2 != side_3 && side_1 != side_3
+      return :scalene
+    else
+      return :isosceles
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end 
+
+
 end
